a40a12b4cc1fdce2628cb9a3c411f0dc66f96a01
Muhammad Shehzad <121470367+malikmuhammadshehzad@users.noreply.github.com>
2025-12-09 13:43:24 +0500

    app icon change and work on android Http issue

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,4 +1,6 @@
 declare module '@env' {
   export const API_URL: string;
+  export const PUBLIC_RC_ANDROID: string;
+  export const PUBLIC_RC_IOS: string;
  
 }
--- src/navigation/Application.tsx
+++ src/navigation/Application.tsx
@@ -25,8 +25,9 @@ import AudioPlayerScreen from '@/screens/TrackPlayerScreen/TrackPlayerScreen';
 import { useAppStore } from '@/store';
 import { useEffect, useState } from 'react';
 import { fetchUserDataLocal } from '@/store/authSlice/authApiService';
-import { ActivityIndicator, View } from 'react-native';
-
+import { ActivityIndicator, Platform, View } from 'react-native';
+import Purchases from 'react-native-purchases';
+import { PUBLIC_RC_ANDROID, PUBLIC_RC_IOS } from '@env';
 const Stack = createStackNavigator<RootStackParamList>();
 
 const AuthStack = () => {
@@ -70,12 +71,9 @@ const HomeStack = () => {
 
 function ApplicationNavigator() {
   const { navigationTheme, variant, fonts, colors } = useTheme();
-  const {userData} = useAppStore(state => state)
+  const { userData } = useAppStore(state => state)
   console.log("🚀 ~ ApplicationNavigator ~ userData:", userData)
-
-  
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const init = async () => {
       setLoading(true);
@@ -84,7 +82,66 @@ function ApplicationNavigator() {
     };
     init();
   }, []);
+  useEffect(() => {
+    const configureRevenueCat = async () => {
+      try {
+        if (userData?.email) {
+          if (Platform.OS === 'ios') {
+            console.log('🚀 ~ RevenueCat configured for IOS!@!123', PUBLIC_RC_IOS);
+            if (!process.env.PUBLIC_RC_IOS) {
+              console.debug('Env Not Found For IOS Cat Revenue!!!');
+            } else {
+              await Purchases.configure({
+                apiKey: process.env.PUBLIC_RC_IOS,
+                appUserID: String(userData?.email),
+              });
+              console.debug('🚀 ~ RevenueCat configured for iOS');
+            }
+            const customerInfo = await Purchases.getCustomerInfo();
+            console.log(
+              '🚀 ~ RootNavigator ~ customerInfo activeSubscriptions:',
+              customerInfo?.activeSubscriptions?.[0],
+            );
+            // if (customerInfo?.activeSubscriptions?.[0]) {
+            //   saveBoolean('SUBSCRIPTION_STATUS', true);
+            //   saveStringStorage('SUBSCRIPTION_PLAN', customerInfo?.activeSubscriptions?.[0]);
+            // } else {
+            //   saveBoolean('SUBSCRIPTION_STATUS', false);
+            //   saveStringStorage('SUBSCRIPTION_PLAN', '');
+            // }
+          } else if (Platform.OS === 'android') {
+            if (!PUBLIC_RC_ANDROID) {
+              console.debug('Env Not Found For Android Cat Revenue');
+            } else {
+              await Purchases.configure({
+                apiKey: process.env.PUBLIC_RC_ANDROID,
+                appUserID: String(userData?.email),
+              });
+            }
+            const customerInfo = await Purchases.getCustomerInfo();
+            console.log(
+              '🚀 ~ RootNavigator ~ customerInfo activeSubscriptions:',
+              customerInfo?.activeSubscriptions?.[0],
+            );
+            // if (customerInfo?.activeSubscriptions?.[0]) {
+            //   saveBoolean('SUBSCRIPTION_STATUS', true);
+            //   saveStringStorage('SUBSCRIPTION_PLAN', customerInfo?.activeSubscriptions?.[0]);
+            // } else {
+            //   saveBoolean('SUBSCRIPTION_STATUS', false);
+            //   saveStringStorage('SUBSCRIPTION_PLAN', '');
+            // }
+          }
+          Purchases.setLogHandler((logLevel, message) => {
+            console.log(`[RevenueCat] ${message}`);
+          });
+        }
+      } catch (error) {
+        console.log('🚀 ~ configureRevenueCat error:', error);
+      }
+    };
 
+    configureRevenueCat();
+  }, [userData?.email]);
   if (loading) {
     return (
       <SafeAreaProvider>
